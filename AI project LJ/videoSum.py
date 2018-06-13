0be8336473a35cafa202ec54417cb147b419e2e3
Juanmi <juanmiblancof@gmail.com>
2018-06-13 17:16:49 +0200

    Added 'recorre_imagenes' function

--- AI project LJ/videoSum.py
+++ AI project LJ/videoSum.py
@@ -1,1 +1,34 @@
-print('NYI')
+import os
+import cv2
+from matplotlib import pyplot as plt
+
+def recorre_imagenes():
+    path = "prueba\\"
+
+    for image in os.listdir(path):
+        input_path = os.path.join(path, image)
+
+        print(input_path)
+
+        img = cv2.imread(input_path)
+
+        cv2.imshow('image', img)
+
+        color = ('b', 'g', 'r')
+
+        for i,col in enumerate(color):
+            histr = cv2.calcHist([img], [i], None, [256], [0,256])
+            plt.plot(histr, color = col)
+            plt.xlim([0,256])
+        
+        plt.show()
+
+if __name__ == '__main__':
+    recorre_imagenes()
+
+
+def CalcularFotogramasClave(INPUTPATH, T, K, H):
+    print("Hola")
+
+
+CalcularFotogramasClave(1,2,3,4)
