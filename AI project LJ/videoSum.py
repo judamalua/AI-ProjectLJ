363cef7464d8cc3dfa0b47aa0bab6fd834a346ef
Juanmi <juanmiblancof@gmail.com>
2018-06-13 19:18:48 +0200

    Added T parameter

--- AI project LJ/videoSum.py
+++ AI project LJ/videoSum.py
@@ -2,33 +2,41 @@ import os
 import cv2
 from matplotlib import pyplot as plt
 
-def recorre_imagenes():
+def recorre_imagenes(T, H):
     path = "C:\\Users\\Juanmi\\Desktop\\Pictures AI project"
+    index = T
+    ListaFrames = list()
 
     for image in os.listdir(path):
-        input_path = os.path.join(path, image)
+        if index != T:
+            index += 1
+        elif index == T:
+            index = 1
+            input_path = os.path.join(path, image)
 
-        print(input_path)
+            print(input_path)
 
-        img = cv2.cv2.imread(input_path)
+            img = cv2.cv2.imread(input_path)
 
-        cv2.cv2.imshow('image', img)
+            cv2.cv2.imshow('image', img)
 
-        color = ('b', 'g', 'r')
+            color = ('b', 'g', 'r')
 
-        for i,col in enumerate(color):
-            histr = cv2.cv2.calcHist([img], [i], None, [256], [0,256])
-            plt.plot(histr, color = col)
-            plt.xlim([0,256])
+            for i,col in enumerate(color):
+                histr = cv2.cv2.calcHist([img], [i], None, [H], [0,256])
+                plt.plot(histr, color = col)
+                plt.xlim([0,256])
+                ListaFrames.append(histr)
         
-        plt.show()
+            plt.show()
 
-if __name__ == '__main__':
-    recorre_imagenes()
+    return ListaFrames
 
 
 def CalcularFotogramasClave(INPUTPATH, T, K, H):
-    print("Hola")
+    if __name__ == '__main__':
+        ListaFrames = recorre_imagenes(T, H)
+        #print(ListaFrames)
 
 
-CalcularFotogramasClave(1,2,3,4)
+CalcularFotogramasClave(1,4,3,256)
