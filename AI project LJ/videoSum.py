02c445969cc3adb93ad1159cedb611595bce8879
Juanmi <juanmiblancof@gmail.com>
2018-06-13 17:38:37 +0200

    sdfg

--- AI project LJ/videoSum.py
+++ AI project LJ/videoSum.py
@@ -10,14 +10,14 @@ def recorre_imagenes():
 
         print(input_path)
 
-        img = cv2.imread(input_path)
+        img = cv2.cv2.imread(input_path)
 
-        cv2.imshow('image', img)
+        cv2.cv2.imshow('image', img)
 
         color = ('b', 'g', 'r')
 
         for i,col in enumerate(color):
-            histr = cv2.calcHist([img], [i], None, [256], [0,256])
+            histr = cv2.cv2.calcHist([img], [i], None, [256], [0,256])
             plt.plot(histr, color = col)
             plt.xlim([0,256])
         
