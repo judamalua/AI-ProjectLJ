361977f99eacbc516b68b50b3dde41bf8b38835b
Juanmi <juanmiblancof@gmail.com>
2018-06-16 20:13:13 +0200

    Starting with calculaKmedias

--- AI project LJ/videoSum.py
+++ AI project LJ/videoSum.py
@@ -1,9 +1,11 @@
 import os
 import cv2
 from matplotlib import pyplot as plt
+import numpy
+from random import randint
 
-def recorre_imagenes(T, H):
-    path = "C:\\Users\\Juanmi\\Desktop\\Pictures AI project"
+def recorre_imagenes(INPUTPATH, T, H):
+    path = INPUTPATH
     index = T
     ListaFrames = list()
 
@@ -18,14 +20,15 @@ def recorre_imagenes(T, H):
 
             img = cv2.cv2.imread(input_path)
 
-            cv2.cv2.imshow('image', img)
+            cv2.cv2.imshow('scene', img)
 
             color = ('b', 'g', 'r')
 
             for i,col in enumerate(color):
-                histr = cv2.cv2.calcHist([img], [i], None, [H], [0,256])
+                histr = cv2.cv2.calcHist([img], [i], None, [H], [0,H])
+                print(histr)
                 plt.plot(histr, color = col)
-                plt.xlim([0,256])
+                plt.xlim([0,H])
                 ListaFrames.append(histr)
         
             plt.show()
@@ -33,10 +36,64 @@ def recorre_imagenes(T, H):
     return ListaFrames
 
 
+def aplicaKmedias(ListaFrames, K, H):
+    ListaFramesConClasificacion = list()
+    centroides = calculaCentroides(K, H)
+
+
+    
+
+
+
+def calculaCentroides(K, H):
+    centroides = list()
+    histr0 = list()
+    histrH = list()
+    
+    for i in range(H):
+        listOfList0 = []
+        if i == 0:
+            listOfList0.append(10000.)
+            histr0.append(listOfList0)
+        else:
+            listOfList0.append(0.)
+            histr0.append(listOfList0)
+
+    for i in range(H):
+        listOfListH = []
+        if i == H - 1:
+            listOfListH.append(10000.)
+            histrH.append(listOfListH)
+        else:
+            listOfListH.append(0.)
+            histrH.append(listOfListH)
+    
+    centroides.append(histr0)
+    centroides.append(histrH)
+
+
+    #print(histr0)
+    #print(histrH)
+    #print(centroides)
+    if K > 2:
+        for index in range(K - 2):
+            histrK = []
+            for j in range(H):
+                listOfListDoubleLoop = []
+                listOfListDoubleLoop.append(randint(0., 10000.))
+                histrK.append(listOfListDoubleLoop)
+        
+            centroides.append(histrK)
+
+    
+    return centroides
+
+
 def CalcularFotogramasClave(INPUTPATH, T, K, H):
     if __name__ == '__main__':
-        ListaFrames = recorre_imagenes(T, H)
+        ListaFrames = recorre_imagenes(INPUTPATH, T, H)
         #print(ListaFrames)
 
 
-CalcularFotogramasClave(1,4,3,256)
+#CalcularFotogramasClave("C:\\Users\\Juanmi\\Desktop\\Pictures AI project",4,3,256)
+aplicaKmedias(list(), 3, 256)
